637602587f24b973a4908e9a1e62754da5bc7684
ZhuoyangM <zhmeng@ucsd.edu>
2022-02-03 19:47:21 -0800

    lib+sh+code(failed)

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-public class SkillDemo{}
+public class SkillDemo{
+    public static int product(int x, int y){
+        return x*y;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    @Test
+    public void testProduct(){
+        assertEquals(3,SkillDemo.product(1, 2));
+    }
+}
